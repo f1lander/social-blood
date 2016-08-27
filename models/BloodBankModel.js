22ef144e9645807d0a11f7bdcfda3efa5db9ff11
f1lander <=>
2016-08-27 10:39:55 -0600

    changin the location type model

--- models/BloodBankModel.js
+++ models/BloodBankModel.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var BloodBankSchema = new Schema({	'name' : String,	'location' : Number,	'contact' : String,	'phone' : String,	'address' : String,	'email' : String});
+var BloodBankSchema = new Schema({
+	name : String,
+	location : { type:[Number], index: '2d', required: true, unique:false},
+	contact : String,
+	phone : String,
+	address : String,
+	email : String
+});
 
 module.exports = mongoose.model('BloodBank', BloodBankSchema);
