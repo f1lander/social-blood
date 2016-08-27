444fc5b06b11aa4f40d12fc747d74ef54f152797
f1lander <=>
2016-08-27 15:11:29 -0600

    allow email for donors flag

--- models/donorModel.js
+++ models/donorModel.js
@@ -4,12 +4,14 @@ var Schema   = mongoose.Schema;
 var donorSchema = new Schema({
 	name : {type: String},
 	lastname : {type: String},
+	phone:{type:String},
 	address:{type:String},
 	location: { type: { type: String, enum: "Point", default: "Point" }, coordinates: { type: [Number], default: [0, 0] } },
 	bloodtype : {type: String},
 	gender:{type:String},
 	age:{type:Number},
-	weight:{type:Number}
+	weight:{type:Number},
+	allowEmail:{type:Boolean}
 });
 
 // mongodb://<dbuser>:<dbpassword>@ds039155.mlab.com:39155/social-blood-db
